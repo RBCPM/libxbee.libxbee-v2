ec6972302ec9fc03053ec868a1dd699b1d1fd4b1
Attie Grande <attie@attie.co.uk>
2011-12-18 23:37:46 +0000

    tidied the plugin sample

--- samples/plugin/main.c
+++ samples/plugin/main.c
@@ -23,18 +23,101 @@
 
 #include <xbee.h>
 
-int main(int argc, char *argv[]) {
-	int i = 5;
+/* this holds onto the ID for the 64-bit data connection */
+unsigned char conType;
+
+/* the callback function */
+void myCB(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **userData) {
+	int ret;
 	
-	xbee_pluginLoad("./plugin.so", NULL);
-	sleep(1);
-	xbee_pluginLoad("./plugin.so", NULL);
-	
-	for (; i; i--) {
-		printf("Hello!! from main() i=%d\n", i);
-		sleep(1);
+	if ((*pkt)->status) {
+		printf("status = %d\n",(*pkt)->status);
+		return;
 	}
-	sleep(1);
+	
+	if ((*pkt)->datalen == 0) {
+		printf("Scan complete!\n");
+		if (userData && *userData) sem_post(*userData);
+		return;
+	}
+
+	if ((*pkt)->datalen < 11) {
+		printf("Short value...\n");
+		return;
+	}
+
+	/* what did the message say? */
+	printf("They said this: %*s\n", (*pkt)->datalen - 11, &((*pkt)->data[10]));
+}
+
+
+int main(int argc, char *argv[]) {
+	int ret;
+	sem_t sem;
+	void *p;
+	
+	/* this is our xbee instance... from 'user' space you don't have access to the struct */
+	struct xbee *xbee;
+	
+	/* this is the connection we will make... again, you don't have access to the struct */
+	struct xbee_con *con;
+	struct xbee_conAddress addr;
+	
+	/* the packet that is recieved... you have access to this! (see xbee.h) */
+	struct xbee_pkt *pkt;
+	
+	/* make a lixbee instance, and connect it to /dev/ttyUSB1 @ 57600 baud
+	   you don't have to keep hold of the returned xbee, in which case you can pass NULL and the most recently started instance will be used! */
+	if ((ret = xbee_setup("/dev/ttyUSB0", 57600, &xbee)) != 0) {
+		xbee_log(NULL,-1,"xbee_setup(): failed... (%d)", ret);
+		exit(1);
+	}
+	
+	/* setup libxbee to use the plugin! */
+	xbee_pluginLoad("./plugin.so", xbee, NULL);
+	
+	/* get the connection type ID, you pass in a string, it returns an ID */
+	if ((ret = xbee_conTypeIdFromName(xbee, "Local AT", &conType)) != 0) {
+		xbee_log(xbee,-1,"xbee_conTypeIdFromName(): failed... (%d)", ret);
+		exit(1);
+	}
+	
+	if ((ret = sem_init(&sem, 0, 0)) != 0) {
+		xbee_log(xbee,-1,"sem_init(): failed... (%d)", ret);
+		exit(1);
+	}
+	
+	/* build a connection to the following address */
+	addr.addr16_enabled = 0;
+	addr.addr64_enabled = 0;
+	if ((ret = xbee_conNew(xbee, &con, conType, &addr, &sem)) != 0) {
+		xbee_log(xbee,-1,"xbee_newcon(): failed... (%d)", ret);
+		exit(1);
+	}
+	{
+    struct xbee_conOptions opts;
+    /* enable waitForAck... this allows us to see if the packet was sent successfully! */
+    xbee_conOptions(xbee, con, &opts, NULL);
+    opts.waitForAck = 1;
+    xbee_conOptions(xbee, con, NULL, &opts);
+	}
+	/* attach the callback */
+	xbee_conAttachCallback(xbee, con, myCB, NULL);
+
+	/* send the request */
+	if ((ret = xbee_conTx(xbee, con, "ND")) != 0) {
+		xbee_log(xbee,-1,"Something went wrong... (%d)", ret);
+	} else {
+		sem_wait(&sem);
+	}
+
+	sem_destroy(&sem);
+
+	/* shutdown the connection */
+	xbee_conEnd(xbee, con, NULL);
+	
+	/* shutdown the libxbee instance */
+	xbee_shutdown(xbee);
 	
 	return 0;
 }
--- samples/plugin/plugin.c
+++ samples/plugin/plugin.c
@@ -19,15 +19,19 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include <xbee.h>
+#include <internal.h>
 
-int myInit(struct xbee *xbee) {
+int myInit(struct xbee *xbee, void *arg) {
+	int ret;
 	printf("Hello!! from myInit() xbee=%p\n", xbee);
+	ret = xbee_modeSet(xbee,"myplugin");
+	printf("xbee_modeSet() returned %d\n", ret);
 	return 0;
 }
 
-void myThread(struct xbee *xbee) {
+void myThread(struct xbee *xbee, void *arg) {
 	int i = 5;
 	for (; i; i--) {
 		printf("Hello!! from myThread() xbee=%p i=%d\n", xbee, i);
@@ -35,8 +39,178 @@ void myThread(struct xbee *xbee) {
 	}
 }
 
+/* ######################################################################### *\
+ * ######################################################################### *
+\* ######################################################################### */
+
+int xbee_sG_atRx(struct xbee *xbee, struct xbee_pktHandler *handler, char isRx, struct bufData **buf, struct xbee_con *con, struct xbee_pkt **pkt) {
+	int ret = XBEE_ENONE;
+	int offset;
+	
+	if (!xbee)         return XBEE_ENOXBEE;
+	if (!handler)      return XBEE_EMISSINGPARAM;
+	if (!isRx)         return XBEE_EINVAL;
+	if (!buf || !*buf) return XBEE_EMISSINGPARAM;
+	if (!con)          return XBEE_EMISSINGPARAM;
+	if (!pkt || !*pkt) return XBEE_EMISSINGPARAM;
+	
+	if ((*buf)->len < 1) {
+		ret = XBEE_ELENGTH;
+		goto die1;
+	}
+	
+	if ((*buf)->buf[0] == 0x88) {
+		offset = 0;
+	} else if ((*buf)->buf[0] == 0x97) {
+		offset = 10;
+		con->address.addr64_enabled = 1;
+		memcpy(con->address.addr64, &((*buf)->buf[2]), 8);
+		con->address.addr16_enabled = 1;
+		memcpy(con->address.addr16, &((*buf)->buf[10]), 2);
+	} else {
+		ret = XBEE_EINVAL;
+		goto die1;
+	}
+	
+	if ((*buf)->len < 5) {
+		ret = XBEE_ELENGTH;
+		goto die1;
+	}
+	
+	con->frameID_enabled = 1;
+	con->frameID = (*buf)->buf[1];
+	
+	(*pkt)->atCommand[0] = (*buf)->buf[offset + 2];
+	(*pkt)->atCommand[1] = (*buf)->buf[offset + 3];
+	
+	(*pkt)->status = (*buf)->buf[offset + 4];
+	
+	(*pkt)->datalen = (*buf)->len - (offset + 5);
+	if ((*pkt)->datalen > 1) {
+		void *p;
+		if ((p = realloc((*pkt), sizeof(struct xbee_pkt) - sizeof(struct xbee_pkt_ioData) + (sizeof(unsigned char) * ((*pkt)->datalen) - 1))) == NULL) {
+			ret = XBEE_ENOMEM;
+			goto die1;
+		}
+	}
+	if ((*pkt)->datalen) memcpy((*pkt)->data, &((*buf)->buf[offset + 5]), (*pkt)->datalen);
+	
+	goto done;
+die1:
+done:
+	return ret;
+}
+
+int xbee_sG_atTx(struct xbee *xbee, struct xbee_pktHandler *handler, char isRx, struct bufData **buf, struct xbee_con *con, struct xbee_pkt **pkt) {
+	int ret = XBEE_ENONE;
+	struct bufData *nBuf;
+	int offset;
+	void *p;
+	
+	if (!xbee)         return XBEE_ENOXBEE;
+	if (!handler)      return XBEE_EMISSINGPARAM;
+	if (isRx)          return XBEE_EINVAL;
+	if (!buf || !*buf) return XBEE_EMISSINGPARAM;
+	if (!con)          return XBEE_EMISSINGPARAM;
+	if (!handler->conType || !handler->conType->txEnabled) return XBEE_EINVAL;
+	if ((*buf)->len < 2) return XBEE_ELENGTH; /* need at least the 2 AT characters! */
+	
+	if ((nBuf = calloc(1, sizeof(struct bufData) + (sizeof(unsigned char) * (XBEE_MAX_PACKETLEN - 1)))) == NULL) {
+		ret = XBEE_ENOMEM;
+		goto die1;
+	}
+	
+	nBuf->buf[0] = handler->conType->txID;
+	if (con->frameID_enabled) {
+		nBuf->buf[1] = con->frameID;
+	}
+	
+	/* local AT (0x09 = queued) */
+	if (nBuf->buf[0] == 0x08 ||
+	    nBuf->buf[0] == 0x09) {
+		offset = 0;
+	
+	/* remote AT */
+	} else if (nBuf->buf[0] == 0x17) {
+		if (con->address.addr64_enabled) {
+			memcpy(&(nBuf->buf[2]), con->address.addr64, 8);
+			nBuf->buf[10] = 0xFF;
+			nBuf->buf[11] = 0xFE;
+		} else if (con->address.addr16_enabled) {
+			/* 64-bit address is ignored if 16-bit address isn't 0xFFFE */
+			if (con->address.addr16[0] == 0xFF &&
+			    con->address.addr16[1] == 0xFE) {
+				ret = XBEE_EINVAL;
+				goto die2;
+			}
+			memcpy(&(nBuf->buf[10]), con->address.addr16, 2);
+		} else {
+			ret = XBEE_EINVAL;
+			goto die2;
+		}
+		if (!con->options.queueChanges) nBuf->buf[12] |= 0x02;
+		offset = 11;
+	
+	} else {
+		ret = XBEE_EINVAL;
+		goto die2;
+	}
+	
+	nBuf->len = offset + 2 + (*buf)->len;
+	if (nBuf->len > XBEE_MAX_PACKETLEN) {
+		ret = XBEE_ELENGTH;
+		goto die2;
+	}
+	memcpy(&(nBuf->buf[offset + 2]), (*buf)->buf, (*buf)->len);
+	
+	/* try (and ignore failure) to realloc buf to the correct length */
+	if ((p = realloc(nBuf, sizeof(struct bufData) + (sizeof(unsigned char) * (nBuf->len - 1)))) != NULL) nBuf = p;
+	
+	*buf = nBuf;
+	
+	goto done;
+die2:
+	free(nBuf);
+die1:
+done:
+	return ret;
+}
+
+/* ######################################################################### *\
+ * ######################################################################### *
+\* ######################################################################### */
+
+/* this links pktHandlers together to form a Tx/Rx capable connection */
+static struct xbee_conType conTypes[] = {
+	ADD_TYPE_RXTX(0x88, 0x08, 0, "Local AT"),
+	
+	ADD_TYPE_TERMINATOR()
+};
+
+/* this links a packet ID with a function */
+static struct xbee_pktHandler pktHandlers[] = {
+	ADD_HANDLER(0x88, xbee_sG_atRx),      /* local AT */
+	ADD_HANDLER(0x08, xbee_sG_atTx),      /* local AT */
+	
+	ADD_HANDLER_TERMINATOR()
+};
+
+/* this links the handlers and the connections into a 'mode' */
+struct xbee_mode my_mode = {
+	pktHandlers: pktHandlers,
+	conTypes: conTypes,
+	name: "myplugin"
+};
+
+/* this is a list of modes that the plugin provides */
+struct xbee_mode *my_libxbee_mode[] = {
+	&my_mode,
+	NULL
+};
+
+/* this is the functionality that this plugin provides */
 struct plugin_features libxbee_features = {
 	.init = myInit,
 	.thread = myThread,
-	.xbee_modes = NULL
+	.xbee_modes = my_libxbee_mode
 };
