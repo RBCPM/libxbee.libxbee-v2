101c5694ed81b1ab131feafd547f70cbf37f0241
Attie Grande <attie@attie.co.uk>
2011-12-18 01:04:02 +0000

    added simple documentation for each function declared in xbee.h... xbee_con*() functions are still to be documented

--- xbee.h
+++ xbee.h
@@ -24,6 +24,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/* this is a list of all the errors that libxbee functions can return */
 #define XBEE_ENONE                                           0
 #define XBEE_EUNKNOWN                                       -1
 #define XBEE_ENOMEM                                         -2
@@ -53,19 +54,26 @@
 #define XBEE_EMUTEX                                        -26
 #define XBEE_EINUSE                                        -27
 
+/* from user-space you don't get access to the xbee or xbee_con structs, and should never de-reference thier pointers... sorry */
 struct xbee;
+struct xbee_con;
 
+/* this struct stores the addressing information for a connection. It should be populated, and passed to xbee_conNew() */
 struct xbee_conAddress {
+	/* 16-bit addressing */
 	unsigned char addr16_enabled;
 	unsigned char addr16[2];
 	
+	/* 64-bit addressing */
 	unsigned char addr64_enabled;
 	unsigned char addr64[8];
 	
+	/* endpoint addressing (series 2 XBees) */
 	unsigned char endpoints_enabled;
 	unsigned char local_endpoint;
 	unsigned char remote_endpoint;
 };
+/* this struct stores the settings for a connection. It should be passed to xbee_conOptions() */
 struct xbee_conOptions {
 	unsigned char disableAck   : 1;
 	unsigned char broadcastPAN : 1;
@@ -74,9 +82,8 @@ struct xbee_conOptions {
 	unsigned char multicast    : 1;
 	unsigned char broadcastRadius;
 };
-struct xbee_con;
 
-
+/* this struct stores a single I/O sample */
 struct xbee_pkt_ioSample {
 	union {
 		struct {
@@ -100,8 +107,8 @@ struct xbee_pkt_ioSample {
 	unsigned short a4;
 	unsigned short a5;
 };
-#define XBEE_IO_DIGITAL_ENABLED_MASK 0x01FF
-#define XBEE_IO_ANALOG_ENABLED_MASK  0x7E00
+
+/* this struct stores the I/O data for a packet. the 'sample' field can be longer than 1, check 'sampleCount' */
 struct xbee_pkt_ioData {
 	unsigned char sampleCount;
 	
@@ -124,40 +131,87 @@ struct xbee_pkt_ioData {
 			unsigned char a5 : 1;
 			unsigned char __space__  : 1;
 		} pin;
+		struct {
+			unsigned short digital : 9; /* this will equate to true if ANY digital inputs are enabled */
+			unsigned short analog  : 6; /* this will equate to true of ANY analog inputs are enabled */
+			unsigned char __space__ : 1;
+		} type;
 		unsigned short mask;
 	} enable;
 
 	struct xbee_pkt_ioSample sample[1];
 };
+
+/* this struct stores the whole packet
+ * 'data[]' should only be accessed if 'data_valid' is TRUE.
+ * 'ioData' should only be accessed if 'ioData_valid' is TRUE.
+ */
 struct xbee_pkt {
 	unsigned char status;
 	unsigned char options;
-	unsigned char rssi;
+	unsigned char rssi; /* the RSSI - if 0x28 (40 dec) is given, the signal strength is -40dBm */
 
 	unsigned char data_valid   : 1;
 	unsigned char ioData_valid : 1;
 
-	int datalen;
 	
 	unsigned char atCommand[2];
 	
+	int datalen; /* if data_valid is TRUE, then this indicates the length of the data stored */
 	/* use EITHER data, or ioData, check the *_valid flags (up) */
 	unsigned char data[1];
 	struct xbee_pkt_ioData ioData;
 };
 
+/* ######################################################################### */
+/* ######################################################################### */
+/* ######################################################################### */
 /* --- xbee.c --- */
-void *xbee_validate(struct xbee *xbee);
+/* this function will return 1 if the given 'xbee' is a valid handle . Otherwise it will return 0 */
+int xbee_validate(struct xbee *xbee);
+
+/* this function will return 0 on success, or an error number on error
+ *-  'path' should be the path to a serial port that is connected to an XBee module
+ *-  'baudrate' should be a standard baud rate, from the list below:
+ *     1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200
+ *-  'retXbee' is an optional field, that allows the developer to start multiple libxbee instances.
+ *     this value should be used to identift which libxbee instance should be used
+` */ 
 int xbee_setup(char *path, int baudrate, struct xbee **retXbee);
+
+/* this function will shutdown the given instance of libxbee and should ALWAYS succeed.
+ *-  'xbee' should be the libxbee instance that you wish to shutdown. If this is NULL, then the most recent instance will be used
+ */
 void xbee_shutdown(struct xbee *xbee);
 
+/* this function will free the given packet memory, and should always succeed
+ *-  'pkt' should be a packet previously returned by xbee_conRx(), or given to a callback function as 'pkt'
+ */
 void xbee_pktFree(struct xbee_pkt *pkt);
 
+/* ######################################################################### */
+/* ######################################################################### */
+/* ######################################################################### */
 /* --- mode.c --- */
+/* this function will provide a list of avaliable libxbee modes
+ * iterate through until you find a NULL pointer, free the returned pointer after use (see samples/modes)
+ */
 char **xbee_modeGetList(void);
+
+/* this function will give the name of the current mode
+ *-  'xbee' should be the libxbee instance that you wish to use. If this is NULL, then the most recent instance will be used
+ */
 char *xbee_modeGet(struct xbee *xbee);
+
+/* this function allows you to set the mode of the given libxbee instance. You MAY re-assign a mode, but data loss is likely
+ *-  'xbee' should be the libxbee instance that you wish to use. If this is NULL, then the most recent instance will be used
+ *-  'name' is the name of the mode you wish to use. This should be one of the avaliable modes, returned by xbee_modeGetList()
+ */
 int xbee_modeSet(struct xbee *xbee, char *name);
 
+/* ######################################################################### */
+/* ######################################################################### */
+/* ######################################################################### */
 /* --- conn.c --- */
 int xbee_conTypeIdFromName(struct xbee *xbee, char *name, unsigned char *id);
 int xbee_conNew(struct xbee *xbee, struct xbee_con **retCon, unsigned char id, struct xbee_conAddress *address, void *userData);
@@ -175,13 +229,30 @@ int xbee_conSetData(struct xbee *xbee, struct xbee_con *con, void *data);
 int xbee_conSleep(struct xbee *xbee, struct xbee_con *con, int wakeOnRx);
 int xbee_conWake(struct xbee *xbee, struct xbee_con *con);
 
+/* ######################################################################### */
+/* ######################################################################### */
+/* ######################################################################### */
 /* --- log.c --- */
+/* this function allows you to specify where the libxbee log should be written to
+ *-  'f' is a valid FILE* that is open for writing. ALL libxbee logging will be written here
+ */
 void xbee_logSetTarget(FILE *f);
+
+/* this function allows you to specify the log level used
+ *-  'level' is the numeric log level. Any messages that have been asigned a value higher than this will not be output
+ */
 void xbee_logSetLevel(int level);
+
 #ifndef __XBEE_INTERNAL_H
+/* this function allows you to write to the libxbee log from user-space, you should use the following macro, and not call this function directly */
 void _xbee_logDev(const char *file, int line, const char *function, int minLevel, char *format, ...);
-#define xbee_log(...) \
-	_xbee_logDev(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
+
+/* this macro allows you to write to the libxbee log
+ *-  'minLevel' the first argument is the log level to use for this message
+ *     this number must be below the level applied using xbee_logSetLevel() for the mssage to appear
+ *-  the remaining arguments are identical to that of printf()
+ */
+#define xbee_log(...) _xbee_logDev(__FILE__, __LINE__, __FUNCTION__, __VA_ARGS__)
 #endif /* __XBEE_INTERNAL_H */
 
 #endif /* __XBEE_H */
