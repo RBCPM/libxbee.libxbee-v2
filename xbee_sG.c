d56530c917ec78e128a7df9a52d9efd90d6a3cb2
Attie Grande <attie@attie.co.uk>
2012-01-14 00:27:17 +0000

    fixed buffering issue

--- xbee_sG.c
+++ xbee_sG.c
@@ -94,7 +94,7 @@ int xbee_sG_atRx(struct xbee *xbee, struct xbee_pktHandler *handler, char isRx, 
 	(*pkt)->status = (*buf)->buf[offset + 4];
 	
 	(*pkt)->datalen = (*buf)->len - (offset + 5);
-	if ((*pkt)->datalen > 1) {
+	if ((*pkt)->datalen > 0) {
 		void *p;
 		if ((p = realloc((*pkt), sizeof(struct xbee_pkt) + (sizeof(unsigned char) * (*pkt)->datalen))) == NULL) {
 			ret = XBEE_ENOMEM;
