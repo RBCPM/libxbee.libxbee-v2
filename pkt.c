RBCPM/libxbee.libxbee-v2

commit 5268eb35083959810d23041168c594f9ab3bd273

commented most of the source code, ironing out a few issues along the way

pkt.c
@@ -26,6 +26,7 @@
 #include "pkt.h"
 #include "ll.h"
 
+/* allocate storage for a new packet */
 struct xbee_pkt *xbee_pktAlloc(void) {
 	struct xbee_pkt *pkt;
 	
@@ -33,14 +34,17 @@ struct xbee_pkt *xbee_pktAlloc(void) {
 		return NULL;
 	}
 	
+	/* this is a pointer, becase struct ll_head is not avaliable in user-space */
 	pkt->dataItems = ll_alloc();
 	
 	return pkt;
 }
 
+/* add a key, or get hold of a key if it exists already */
 int xbee_pktAddKey(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, struct pkt_infoKey **retKey, void (*freeCallback)(void*)) {
 	struct pkt_infoKey *p;
 	
+	/* check parameters */
 	if (!xbee) {
 		if (!xbee_default) return XBEE_ENOXBEE;
 		xbee = xbee_default;
@@ -50,36 +54,45 @@ int xbee_pktAddKey(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, s
 	if (!key) return XBEE_EMISSINGPARAM;
 	if (!retKey) return XBEE_EMISSINGPARAM;
 	
+	/* check if the key already exists */
 	if (xbee_pktGetKey(xbee, pkt, key, id, &p) == 0) {
+		/* if it does return it, and notify the caller that it was already in existance */
 		*retKey = p;
 		return XBEE_EEXISTS;
 	}
 	
+	/* wipeout the retKey, just incase something fails */
 	*retKey = NULL;
 	
+	/* allocate some storage */
 	if ((p = calloc(1, sizeof(struct pkt_infoKey))) == NULL) {
 		return XBEE_ENOMEM;
 	}
 	
+	/* store the key's name, and other details */
 	snprintf(p->name, PKT_INFOKEY_MAXLEN, "%s", key);
-	p->id = id;
-	p->freeCallback = freeCallback;
+	p->id = id; /* the 'id' can be used to identify one 'analog' key from another (e.g. different channels) */
+	p->freeCallback = freeCallback; /* how to free the data that will be stored on this key */
 	ll_init(&p->items);
 	
+	/* add the key to the packet */
 	if (ll_add_tail(pkt->dataItems, p)) {
 		free(p);
 		return XBEE_ENOMEM;
 	}
 	
+	/* and return it */
 	*retKey = p;
 	
 	return 0;
 }
 
+/* add info to a packet */
 int xbee_pktAddInfo(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, void *data, void (*freeCallback)(void*)) {
 	int ret;
 	struct pkt_infoKey *p;
 	
+	/* check parameters */
 	if (!xbee) {
 		if (!xbee_default) return XBEE_ENOXBEE;
 		xbee = xbee_default;
@@ -88,10 +101,12 @@ int xbee_pktAddInfo(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, 
 	if (!pkt) return XBEE_EMISSINGPARAM;
 	if (!key) return XBEE_EMISSINGPARAM;
 	
+	/* add or retrieve an existing key */
 	if ((ret = xbee_pktAddKey(xbee, pkt, key, id, &p, freeCallback)) != 0 && ret != XBEE_EEXISTS) {
 		return ret;
 	}
 	
+	/* add the data to the key's list */
 	if (ll_add_tail(&p->items, data)) {
 		return XBEE_ELINKEDLIST;
 	}
@@ -99,18 +114,22 @@ int xbee_pktAddInfo(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, 
 	return 0;
 }
 
+/* this is a wrapper to handle analog samples, it will add an analog sample to the packet */
 int xbee_pktAddAnalog(struct xbee *xbee, struct xbee_pkt *pkt, int channel, int value) {
 	int ret;
 	
+	/* add the data, here the linked list's pointer is actually used as a VALUE, a little naughty but needs no callback */
 	if ((ret = xbee_pktAddInfo(xbee, pkt, "analog", channel, (void*)((long)value), NULL)) != 0) {
 		return ret;
 	}
 	
 	return 0;
 }
+/* this is a wrapper to handle digital samples, it will add a digital sample to the packet */
 int xbee_pktAddDigital(struct xbee *xbee, struct xbee_pkt *pkt, int channel, int value) {
 	int ret;
 	
+	/* add the data, here the linked list's pointer is actually used as a VALUE, a little naughty but needs no callback */
 	if ((ret = xbee_pktAddInfo(xbee, pkt, "digital", channel, (void*)((long)value), NULL)) != 0) {
 		return ret;
 	}
@@ -118,9 +137,11 @@ int xbee_pktAddDigital(struct xbee *xbee, struct xbee_pkt *pkt, int channel, int
 	return 0;
 }
 
+/* get a key from the packet */
 int xbee_pktGetKey(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, struct pkt_infoKey **retKey) {
 	struct pkt_infoKey *p;
 	
+	/* check parameters */
 	if (!xbee) {
 		if (!xbee_default) return XBEE_ENOXBEE;
 		xbee = xbee_default;
@@ -130,20 +151,26 @@ int xbee_pktGetKey(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, s
 	if (!key) return XBEE_EMISSINGPARAM;
 	
 	*retKey = NULL;
+	/* find the key */
 	for (p = NULL; (p = ll_get_next(pkt->dataItems, p)) != NULL;) {
 		if (!strncasecmp(key, p->name, PKT_INFOKEY_MAXLEN)) {
+			/* if id is specified as -1, then this will match ANY id that is found, use with care! */
 			if (id == -1 || p->id == id) {
 				*retKey = p;
 				return 0;
 			}
 		}
 	}
+	
+	/* if we get here, then the key doesnt exist */
 	return XBEE_EFAILED;
 }
 
+/* get info from a packet */
 int xbee_pktGetInfo(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, int index, void **retItem) {
 	struct pkt_infoKey *p;
 	
+	/* check parameters */
 	if (!xbee) {
 		if (!xbee_default) return XBEE_ENOXBEE;
 		xbee = xbee_default;
@@ -155,14 +182,17 @@ int xbee_pktGetInfo(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, 
 	
 	*retItem = NULL;
 	
+	/* get the key, or fail */
 	if (xbee_pktGetKey(xbee, pkt, key, id, &p)) {
 		return XBEE_EINVAL;
 	}
 	
+	/* check that the index is within the bounds of the list, or fail */
 	if (index >= ll_count_items(&p->items)) {
 		return XBEE_ERANGE;
 	}
 	
+	/* get the item, or fail */
 	if ((*retItem = ll_get_index(&p->items, index)) == NULL) {
 		return XBEE_ENULL;
 	}
@@ -170,38 +200,51 @@ int xbee_pktGetInfo(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, 
 	return 0;
 }
 
+/* get the analog data for 'channel' and 'sample'/'index' from the packet */
 EXPORT int xbee_pktGetAnalog(struct xbee *xbee, struct xbee_pkt *pkt, int channel, int index, int *retVal) {
 	void *val;
 	int ret;
 	
+	/* this is a simeple retrieve! */
 	if (((ret = xbee_pktGetInfo(xbee, pkt, "analog", channel, index, &val)) != 0) && ret != XBEE_ENULL) {
 		return ret;
 	}
 	
+	/* give the cast value back */
 	*retVal = (long)val;
 	return 0;
 }
 
+/* get the digital data for 'channel' and 'sample'/'index' from the packet */
 EXPORT int xbee_pktGetDigital(struct xbee *xbee, struct xbee_pkt *pkt, int channel, int index, int *retVal) {
 	void *val;
 	int ret;
 	
+	/* this is a simeple retrieve! */
 	if (((ret = xbee_pktGetInfo(xbee, pkt, "digital", channel, index, &val)) != 0) && ret != XBEE_ENULL) {
 		return ret;
 	}
 	
-	*retVal = !!val;
+	/* give the cast value back (it's digital remember, 0 or 1, so for simplicity lets keep it that way) */
+	*retVal = !!((long)val);
 	return 0;
 }
 
-static inline void xbee_pktFreeData(struct pkt_infoKey *key) {
+/* free a packet's dataItem, this can be called from ll_free() */
+static void xbee_pktFreeData(struct pkt_infoKey *key) {
 	ll_destroy(&key->items, key->freeCallback);
 	free(key);
 }
+/* free a packet's resources
+   simply calling free() on the packet will work, though you will leak memory */
 EXPORT void xbee_pktFree(struct xbee_pkt *pkt) {
+#warning TODO - store packets in a 'released' list after calling xbee_rx()
+	/* check parameters - we can't do more than this, as the packet has been unlinked by the time the user gets hands on */
 	if (!pkt) return;
 	
+	/* free all of the dataItems */
 	if (pkt->dataItems) ll_free(pkt->dataItems, (void(*)(void*))xbee_pktFreeData);
 	
+	/* win32 implementations must have free() called from within the library... doh */
 	free(pkt);
 }

rx.c
@@ -40,6 +40,7 @@ struct xbee_callbackInfo {
 
 /* ######################################################################### */
 
+/* the callback thread! */
 int _xbee_rxCallbackThread(struct xbee_callbackInfo *info) {
 	struct xbee_pkt *pkt, *opkt;
 	struct xbee *xbee;
@@ -49,17 +50,21 @@ int _xbee_rxCallbackThread(struct xbee_callbackInfo *info) {
 	/* prevent having to xsys_thread_join() */
 	xsys_thread_detach_self();
 	
+	/* store the info */
 	xbee = info->xbee;
 	con = info->con;
+	/* and mark it as used - the data is stored in a local variable in xbee_triggerCallback() */
 	info->taken = 1;
 	
+	/* mark us as running */
 	con->callbackRunning = 1;
 	
-	for (;;) {
-		if (con->destroySelf) break;
+	while (!con->destroySelf) {
+		/* get the next packet */
 		pkt = ll_ext_head(&(con->rxList));
 		if (!pkt) {
 			int semval;
+			/* if there isnt a 'next packet', then wait for one */
 			if (xsys_sem_getvalue(&con->callbackSem, &semval) != 0) {
 				xbee_log(1,"xsys_sem_getvalue(): Error while retrieving value...");
 				break;
@@ -75,30 +80,41 @@ int _xbee_rxCallbackThread(struct xbee_callbackInfo *info) {
 				}
 				continue;
 			}
+			/* otherwise just wait paitently for 5 seconds... */
 			if (xsys_sem_timedwait(&con->callbackSem, 5, 0) != 0) {
 				if (errno != ETIMEDOUT) {
 					xbee_log(1,"xsys_sem_timedwait(): Error while waiting... (%d)", errno);
 				}
+				/* ... and then kill of the thread (waiting helps to prevent thrashing, and still keep resource usage down) */
 				break;
 			}
+			/* oo! we got prodded! */
 			continue;
 		}
 		
+		/* get hold of the callback function
+		   this is done each time round, so that updates take effect without having to kill and restart the thread */
 		callback = con->callback;
 		if (!callback) {
 			xbee_log(1,"Callback for connection @ %p disappeared... replacing the packet", con);
 			ll_add_head(&(con->rxList), pkt);
 			break;
 		}
+		
 		xbee_log(1,"Running callback (func: %p, xbee: %p, con: %p, pkt: %p, userData: %p)",
 		                              callback, xbee, con, pkt, con->userData);
 
+		/* keep hold of the packet's original address - opkt */
 		opkt = pkt;
+		/* run the callback */
 		callback(xbee, con, &pkt, &con->userData);
 		if (pkt) {
+			/* if the developer wants to hold onto the packet themselves, then they should set pkt to NULL, otherwise this will happen */
 			if (pkt != opkt) {
+				/* if the pointer has changed, we shouldn't trust it... */
 				xbee_log(-10,"Connection callback for con: %p returned different packet... not attempting to free unknown pointer", con);
 			} else {
+				/* if the pointer is still the same, then free the packet */
 				xbee_pktFree(pkt);
 			}
 		} else {
@@ -106,20 +122,25 @@ int _xbee_rxCallbackThread(struct xbee_callbackInfo *info) {
 		}
 	}
 	
+	/* when we die, log the fact */
 	xbee_log(2,"Callback thread terminating (con: %p)", con);
 	
+	/* mark us as not-running */
 	con->callbackRunning = 0;
 
+	/* if the connection has been marked to 'destroySelf', then we need to finish tidying up the connection */
 	if (con->destroySelf) {
-		xbee_conFree(xbee, con);
+		_xbee_conEnd2(xbee, con);
 	}
 
 	return 0;
 }
 
+/* this is a magical function that starts the callback thread for a connection */
 void xbee_triggerCallback(struct xbee *xbee, struct xbee_con *con) {
+	/* if the thread isn't marked as running, or even started then start it */
 	if ((!con->callbackStarted || !con->callbackRunning)) {
-		struct xbee_callbackInfo info;
+		struct xbee_callbackInfo info;  	                                           /* vv */
 		info.xbee = xbee;
 		info.con = con;
 		info.taken = 0;
@@ -127,16 +148,19 @@ void xbee_triggerCallback(struct xbee *xbee, struct xbee_con *con) {
 		con->callbackRunning = 0;
 		if (!xsys_thread_create(&con->callbackThread, (void*(*)(void*))_xbee_rxCallbackThread, (void*)&info)) {
 			con->callbackStarted = 1;
+			/* wait for the info to be taken - its that local variable up there remember  ^^ */
 			while (!info.taken) {
 				usleep(100);
 			}
 		} else {
-			xbee_log(-999,"Failed to start callback for connection @ %p... xsys_thread_create() returned error", con);
+			xbee_log(-99,"Failed to start callback for connection @ %p... xsys_thread_create() returned error", con);
 		}
 	}
+	/* prod it just incase */
 	xsys_sem_post(&con->callbackSem);
 }
 
+/* this thread is thread is activated for each pktHandler that recieves data */
 int _xbee_rxHandlerThread(struct xbee_pktHandler *pktHandler) {
 	int ret;
 	struct rxData *data;
@@ -149,65 +173,77 @@ int _xbee_rxHandlerThread(struct xbee_pktHandler *pktHandler) {
 	/* prevent having to xsys_thread_join() */
 	xsys_thread_detach_self();
 	
+	/* check parameters */
 	if (!pktHandler) return XBEE_EMISSINGPARAM;
 	data = pktHandler->rxData;
 	if (!data) return XBEE_EMISSINGPARAM;
 	xbee = data->xbee;
 	if (!xbee) return XBEE_ENOXBEE;
 	
+	/* mark ourselves as running */
 	data->threadRunning = 1;
 	
 	pkt = NULL;
 	for (;!data->threadShutdown;) {
+		/* wait for a packet */
 		if (xsys_sem_wait(&data->sem)) {
 			xbee_perror(1,"xsys_sem_wait()");
 			usleep(100000);
 			continue;
 		}
 		
+		/* sniff it up */
 		buf = ll_ext_head(&data->list);
 		if (!buf) {
+			/* oh... well lets go back to sleep */
 			xbee_log(1,"No buffer!");
 			continue;
 		}
-		
-		if (!pkt) {
-			/* only allocate memory if nessesary (re-use where possible) */
-			if ((pkt = xbee_pktAlloc()) == NULL) {
-				xbee_perror(1,"xbee_pktAlloc()");
-				usleep(100000);
-				continue;
-			}
+
+		/* make space for a new packet */
+		if ((pkt = xbee_pktAlloc()) == NULL) {
+			xbee_perror(1,"xbee_pktAlloc()");
+			usleep(100000);
+			continue;
 		}
 		
 		xbee_log(2,"Processing packet @ %p", buf);
 		
+		/* clear out the connection and packet structs - the handler should fill them in */
+		memset( pkt, 0, sizeof(struct xbee_pkt));
 		memset(&con, 0, sizeof(struct xbee_con));
+		/* call the handler, it should fill in con and pkt */
 		if ((ret = pktHandler->handler(data->xbee, pktHandler, 1, &buf, &con, &pkt)) != 0) {
 			xbee_log(1,"Failed to handle packet... pktHandler->handler() returned %d", ret);
 			goto skip;
 		}
 		if (!pkt) {
-			xbee_log(1,"pktHandler->handler() failed to return a packet!");
+			xbee_log(1,"pktHandler->handler() failed to return a packet! This has quite possibly caused a memory leak...");
 			goto skip;
 		}
 		
+		/* if a frameID was provided, then poke the waiting thread */
 		if (con.frameID_enabled) {
 			xbee_frameIdGiveACK(xbee, con.frameID, pkt->status);
 		}
 		
+		/* if the conType demands an address, but we haven't got one, then skip the rest */
 		if (pktHandler->conType->needsAddress &&
 		    !con.address.addr16_enabled &&
 		    !con.address.addr64_enabled) goto skip;
-		
+
+		/* log the address we recieved the packet for */
 		xbee_conLogAddress(xbee, &con.address);
 		
+		/* get a connection */
 		if ((rxCon = xbee_conFromAddress(xbee, pktHandler->conType, &con.address)) == NULL) {
 			xbee_log(3,"No connection for packet...");
 			goto skip;
 		}
+		/* if it is sleeping, then wake it up */
 		if (rxCon->sleeping) {
 			if (!rxCon->wakeOnRx) {
+				/* unless it is sleeping 'deeply' */
 				xbee_log(3,"Found a connection @ %p, but it's in a 'deep sleep'...", rxCon);
 				goto skip;
 			}
@@ -215,9 +251,11 @@ int _xbee_rxHandlerThread(struct xbee_pktHandler *pktHandler) {
 			rxCon->sleeping = 0;
 		}
 		
+		/* add the packet to the connections rxList */
 		ll_add_tail(&rxCon->rxList, pkt);
 		
 		if (rxCon->callback) {
+			/* trigger a callback if appropriate */
 			xbee_triggerCallback(data->xbee, rxCon);
 		}
 		
@@ -226,6 +264,7 @@ int _xbee_rxHandlerThread(struct xbee_pktHandler *pktHandler) {
 		/* flag pkt for a new allocation */
 		pkt = NULL;
 skip:
+		/* free up any storage */
 		if (pkt) xbee_pktFree(pkt);
 		pkt = NULL;
 		if (buf) free(buf);
@@ -233,23 +272,29 @@ skip:
 	
 	if (pkt) xbee_pktFree(pkt);
 
+	/* mark us as not running */
 	data->threadRunning = 0;
 	return 0;
 }
 
+/* handle the pktHandler's rxData, start a handler thread if there isn't one running, and then poke the handler thread */
 int _xbee_rxHandler(struct xbee *xbee, struct xbee_pktHandler *pktHandler, struct bufData *buf) {
 	int ret = XBEE_ENONE;
 	struct rxData *data;
 	
+	/* check parameters */
 	if (!xbee) return XBEE_ENOXBEE;
 	if (!pktHandler) return XBEE_EMISSINGPARAM;
 	if (!buf) return XBEE_EMISSINGPARAM;
+	
 	data = pktHandler->rxData;
 	if (!data) {
+		/* setup some rxData if there isn't any yet */
 		if (!(data = calloc(1, sizeof(struct rxData)))) {
 			ret = XBEE_ENOMEM;
 			goto die1;
 		}
+		/* keep hold of the xbee instance */
 		data->xbee = xbee;
 		if (xsys_sem_init(&data->sem)) {
 			ret = XBEE_ESEMAPHORE;
@@ -259,9 +304,11 @@ int _xbee_rxHandler(struct xbee *xbee, struct xbee_pktHandler *pktHandler, struc
 			ret = XBEE_ELINKEDLIST;
 			goto die3;
 		}
+		/* assign the rxData to the pktHandler */
 		pktHandler->rxData = data;
 	}
 	
+	/* start a handler thread if necessary */
 	if (!data->threadStarted || !data->threadRunning) {
 		data->threadRunning = 0;
 		if (xsys_thread_create(&data->thread, (void*(*)(void*))_xbee_rxHandlerThread, (void*)pktHandler)) {
@@ -272,6 +319,7 @@ int _xbee_rxHandler(struct xbee *xbee, struct xbee_pktHandler *pktHandler, struc
 		data->threadStarted = 1;
 	}
 	
+	/* add the buffer to the list, and poke the thread */
 	ll_add_tail(&data->list, buf);
 	xsys_sem_post(&data->sem);
 	
@@ -283,10 +331,12 @@ die3:
 die2:
 	free(data);
 die1:
+	pktHandler->rxData = NULL;
 done:
 	return ret;
 }
 
+/* the XBee serial Rx function */
 int xbee_rxSerialXBee(struct xbee *xbee, struct bufData **buf, int retries) {
 	struct bufData *ibuf;
 	int pos;
@@ -297,25 +347,39 @@ int xbee_rxSerialXBee(struct xbee *xbee, struct bufData **buf, int retries) {
 
 	ret = XBEE_ENONE;
 
+	/* make space to recieve the packet */
 	if ((ibuf = calloc(1, sizeof(struct bufData) + (sizeof(unsigned char) * (XBEE_MAX_PACKETLEN - 1)))) == NULL) {
 		ret = XBEE_ENOMEM;
 		goto die1;
 	}
+	/* keep hold of it in the xbee instance, to prevent memory leak on xbee_shutdown() */
 	xbee->rxBuf = (void*)ibuf;
 	
+	/* read the packet
+	     -3 = Start of frame (0x7E)
+	     -2 = length (msb)
+	     -1 = length (lsb)
+	      0 = API identifier
+	    ... = data
+	      n = checksum */
 	for (pos = -3; pos < 0 || (pos < len && pos < XBEE_MAX_PACKETLEN); pos++) {
+	
+		/* get the data, byte by byte */
 		if ((ret = xbee_io_getEscapedByte(xbee, &c)) != 0) {
+			
 			if (ret == XBEE_EUNESCAPED_START) {
 				if (pos > -3) xbee_log(3,"Unexpected start byte... restarting packet capture");
-				pos = -3;
+				pos = -3; /* reset to the begining */
 				continue;
 			} else if (ret == XBEE_EEOF) {
+				/* EOF seems to occur when USB devices are unplugged */
 				if (--retries == 0) {
 					xbee_log(1,"Too many device failures (EOF)");
 					goto die2;
 				}
 				/* try closing and re-opening the device */
 				usleep(100000);
+				/* this will most likely fail (because the device has been removed) */
 				if ((ret = xbee_io_reopen(xbee)) != 0) {
 					ret = XBEE_EOPENFAILED;
 					goto die2;
@@ -323,10 +387,13 @@ int xbee_rxSerialXBee(struct xbee *xbee, struct bufData **buf, int retries) {
 				usleep(10000);
 				continue;
 			}
+			/* otherwise there was an unknown error */
 			xbee_perror(1,"xbee_io_getEscapedByte()");
 			ret = XBEE_EIO;
 			goto die2;
 		}
+		
+		/* pull in the data as relevant */
 		switch (pos) {
 			case -3:
 				if (c != 0x7E) pos--;    /* restart if we don't have the start of frame */
@@ -341,12 +408,12 @@ int xbee_rxSerialXBee(struct xbee *xbee, struct bufData **buf, int retries) {
 				chksum = 0;              /* wipe the checksum */
 				break;
 			default:
-				chksum += c;
-				ibuf->buf[pos] = c;
+				chksum += c;             /* keep track of the checksum */
+				ibuf->buf[pos] = c;      /* pull in the data */
 		}
 	}
 	
-  /* check the checksum */
+  /* check the checksum (should = 0xFF) */
   if ((chksum & 0xFF) != 0xFF) {
 		int i;
    	xbee_log(1,"Invalid checksum detected... %d byte packet discarded", ibuf->len);
@@ -357,7 +424,9 @@ int xbee_rxSerialXBee(struct xbee *xbee, struct bufData **buf, int retries) {
 		goto die2;
   }
 
+	/* return the buffer */
 	*buf = ibuf;
+	xbee->rxBuf = NULL;
 	goto done;
 die2:
 	free(ibuf);
@@ -366,8 +435,8 @@ done:
 	return ret;
 }
 
+/* recieve buffers, one by one, find a handler and send them on thier way */
 int _xbee_rx(struct xbee *xbee) {
-  static int warnedMode = 0;
 	struct bufData *buf;
 	void *p;
 	int pos;
@@ -375,38 +444,41 @@ int _xbee_rx(struct xbee *xbee) {
 	int ret;
 	struct xbee_pktHandler *pktHandlers;
 	
+	/* check parameters */
 	if (!xbee) return XBEE_ENOXBEE;
 
-	if (!xbee->mode && !warnedMode) usleep(1000);
+	/* ensure we have a mode */
 	if (!xbee->mode) {
-		if (!warnedMode) {
-      xbee_log(-99,"libxbee's mode has not been set, please use xbee_setMode()");
-      warnedMode = 1;
-    }
+		xbee_log(-99,"libxbee's mode has not been set, please use xbee_setMode()");
+		sleep(1);
 		return XBEE_ENOMODE;
 	}
-  warnedMode = 0;
 	
 	buf = NULL;
 	while (xbee->running) {
 		ret = XBEE_ENONE;
+		
+		/* if there isnt an rx function mapped, then we can't do much here! */
 		if (!xbee->f->rx) {
-			xbee_log(-999, "xbee->f->rx(): not registered!");
+			xbee_log(-99, "xbee->f->rx(): not registered!");
 			return XBEE_EINVAL;
 		}
 		
 		buf = NULL;
+		/* get a buffer */
 		if ((ret = xbee->f->rx(xbee, &buf, retries)) != 0) {
 			goto die1;
 		}
 		
+		/* if we have no mode, then we have to die... */
 		if (!xbee->mode) {
-			xbee_log(-999,"libxbee's mode has been un-set, please use xbee_setMode()");
+			xbee_log(-99,"libxbee's mode has been un-set, please use xbee_setMode()");
 			ret = XBEE_ENOMODE;
 			goto die2;
 		}
 		pktHandlers = xbee->mode->pktHandlers;
 		
+		/* find an initialized packet handler that should be able to handle this message */
 		for (pos = 0; pktHandlers[pos].handler; pos++) {
 			if (!pktHandlers[pos].initialized) continue;
 			if (pktHandlers[pos].id == buf->buf[0]) break;
@@ -420,7 +492,6 @@ int _xbee_rx(struct xbee *xbee) {
 		/* try (and ignore failure) to realloc buf to the correct length */
 		if ((p = realloc(buf, sizeof(struct bufData) + (sizeof(unsigned char) * (buf->len - 1)))) != NULL) buf = p;
 
-		xbee->rxBuf = NULL;
 		if ((ret = _xbee_rxHandler(xbee, &pktHandlers[pos], buf)) != 0) {
 			xbee_log(1,"Failed to handle packet... _xbee_rxHandler() returned %d", ret);
 			free(buf);
@@ -433,23 +504,38 @@ int _xbee_rx(struct xbee *xbee) {
 
 die2:
 	free(buf);
-	xbee->rxBuf = NULL;
 die1:
 done:
 	return ret;
 }
 
+/* rx thread */
 int xbee_rx(struct xbee *xbee) {
 	int ret;
+	
+	/* ensure we have an xbee instance */
 	if (!xbee) return 1;
 	
+	/* indicate that we are running */
 	xbee->rxRunning = 1;
+	
 	while (xbee->running) {
+		/* keep executing the rx function */
 		ret = _xbee_rx(xbee);
 		if (ret != -19) xbee_log(1,"_xbee_rx() returned %d", ret);
+		
 		if (!xbee->running) break;
-		usleep(XBEE_RX_RESTART_DELAY * 1000);
+		
+		if (ret && XBEE_RX_RESTART_DELAY < 2000) {
+			/* if an error occured (returned non-zero), sleep for at least 2 seconds */
+			sleep(2);
+		} else {
+			/* otherwise sleep for RESTART_DELAY ms (default: 25ms) */
+			usleep(XBEE_RX_RESTART_DELAY * 1000);
+		}
 	}
+	
+	/* indicate that we are no longer running */
 	xbee->rxRunning = 0;
 	
 	return 0;

HISTORY
@@ -13,6 +13,7 @@ v2.0.5 - in progress
 		+ Removed 'mapping' functions from xsys_linux.c, and added mapping macros instead (saved ~1.5k)
 		+ Implemented xbee_pluginUnload() and added pluginData storage for plugins
 		+ xbee_conNew() now returns XBEE_EEXISTS if a connection already exists (still returns the *con)
+		+ Commented most of the source code, ironing out a few issues along the way
 
 v2.0.4 - ada265100533 - 31 Dec 2011
 	Modifications / Additions:

conn.h
@@ -30,6 +30,8 @@ struct xbee_conType *_xbee_conTypeFromID(struct xbee_conType *conTypes, unsigned
 struct xbee_conType *xbee_conTypeFromID(struct xbee_conType *conTypes, unsigned char id);
 struct xbee_con *xbee_conFromAddress(struct xbee *xbee, struct xbee_conType *conType, struct xbee_conAddress *address);
 
+int _xbee_conEnd2(struct xbee *xbee, struct xbee_con *con);
+
 int _xbee_conValidate(struct xbee *xbee, struct xbee_con *con, struct xbee_conType **conType);
 
 void xbee_conLogAddress(struct xbee *xbee, struct xbee_conAddress *address);

fmaps.c
@@ -29,6 +29,7 @@
 #include "tx.h"
 #include "io.h"
 
+/* this is the default function map, used by all SERIAL xbee units */
 const struct xbee_fmap xbee_fmap_serial = {
 	.io_open = xbee_io_open,
 	.io_close = xbee_io_close,
@@ -39,6 +40,7 @@ const struct xbee_fmap xbee_fmap_serial = {
 	.postInit = NULL,
 	.shutdown = xbee_shutdown,
 
+	/* these don't need extending / redirecting at all */
 	.conValidate = NULL,
 	.conNew = NULL,
 	.connTx = NULL,

frame.c
@@ -26,45 +26,69 @@
 #include "internal.h"
 #include "frame.h"
 
+/* get a free FrameID for message transmission */
 unsigned char xbee_frameIdGet(struct xbee *xbee, struct xbee_con *con) {
 	unsigned char i;
 	unsigned char ret;
 	ret = 0;
+	
+	/* lock the array of IDs */
 	xsys_mutex_lock(&xbee->frameIdMutex);
+	
+	/* find one that isn't being used, we start at the one after the last one provided (it'll probrably be free)
+	   scary use of 'i' here... it is an unsigned char so should wrap around at 255... */
 	for (i = xbee->frameIdLast + 1; i != xbee->frameIdLast; i++) {
+		/* skip FrameID 0 (it indicates to the XBee units that no ACK is requested) */
 		if (i == 0) i++;
+		
+		/* if we have been in a full circle, then return (none found) */
 		if (i == xbee->frameIdLast) break;
 		
+		/* if this FrameID doesn't have a connection assigned to it, then it is free! */
 		if (!xbee->frameIds[i].con) {
+			/* set the ack state to unknown, and aquire the FrameId */
 			xbee->frameIds[i].ack = XBEE_EUNKNOWN;
 			xbee->frameIds[i].con = con;
-			xbee->frameIdLast = i;
+			/* update the last, so that future hunting should be quicker */
+			xbee->frameIdLast = i + 1;
+			/* return the FrameID assigned */
 			ret = i;
 			break;
 		}
 	}
+	
+	/* unlock the array! */
 	xsys_mutex_unlock(&xbee->frameIdMutex);
+	
 	return ret;
 }
 
+/* give an ACK to a FrameID */
 void xbee_frameIdGiveACK(struct xbee *xbee, unsigned char frameID, unsigned char ack) {
 	struct xbee_frameIdInfo *info;
-	if (!xbee)          return;
+	/* very basic checking of parameters */
+	if (!xbee)            return;
 	info = &(xbee->frameIds[frameID]);
-	if (!info->con)     return;
+	/* just to ensure that the FrameID is actually in use */
+	if (!info->con)       return;
+	
+	/* provide the ACK value */
 	info->ack = ack;
+	/* and prod the waiter */
 	xsys_sem_post(&info->sem);
 }
 
+/* wait for an ACK, and retrieve it */
 int xbee_frameIdGetACK(struct xbee *xbee, struct xbee_con *con, unsigned char frameID) {
 	struct xbee_frameIdInfo *info;
 	int ret;
-	if (!xbee)          return XBEE_ENOXBEE;
-	if (!con)           return XBEE_EMISSINGPARAM;
+	/* very basic checking of parameters */
+	if (!xbee)            return XBEE_ENOXBEE;
+	if (!con)             return XBEE_EMISSINGPARAM;
 	info = &(xbee->frameIds[frameID]);
-	if (info->con != con) {
-		return XBEE_EINVAL;
-	}
+	if (info->con != con) return XBEE_EINVAL;
+	
+	/* wait for AT MOST 1 second! that should REALLY be enough, but also prevent blocking indefinately */
 	if (xsys_sem_timedwait(&info->sem,1,0)) {
 		if (errno == ETIMEDOUT) {
 			ret = XBEE_ETIMEOUT;
@@ -73,8 +97,12 @@ int xbee_frameIdGetACK(struct xbee *xbee, struct xbee_con *con, unsigned char fr
 		}
 		goto done;
 	}
+	
+	/* give some useful information back to the user! */
 	ret = info->ack;
 done:
+
+	/* free up the FrameID, this will also prevent a sem_wait() from occuring on an abandoned (timeout) FrameID */
 	info->con = NULL;
 	return ret;
 }

internal.h
@@ -94,7 +94,7 @@ struct xbee_fmap {
 	int  (*conNew)(struct xbee *xbee, struct xbee_con **retCon, unsigned char id, struct xbee_conAddress *address, void *userData); /* extension */
 	/* conRx() currently doesn't need any function mapping */
 	int  (*connTx)(struct xbee *xbee, struct xbee_con *con, struct bufData *buf); /* extension & diversion */
-	int  (*conEnd)(struct xbee *xbee, struct xbee_con *con, void **userData); /* extension */
+	int  (*conEnd)(struct xbee *xbee, struct xbee_con *con); /* extension */
 	int  (*conOptions)(struct xbee *xbee, struct xbee_con *con, struct xbee_conOptions *getOptions, struct xbee_conOptions *setOptions); /* extension */
 	int  (*conSleep)(struct xbee *xbee, struct xbee_con *con, int wakeOnRx); /* extension */
 	int  (*conWake)(struct xbee *xbee, struct xbee_con *con); /* extension */

commit 5a2bfdbd1cf583ae0b50cc9c42bfae792bfe5c82

fixed missing dataItems issue (introduced in 5268eb35083959810d23041168c594f9ab3bd273)

pkt.c
@@ -40,6 +40,14 @@ struct xbee_pkt *xbee_pktAlloc(void) {
 	return pkt;
 }
 
+/* clean a packet (doesn't consider data past the struct's size, e.g. the 'data' item) */
+void xbee_pktClean(struct xbee_pkt *pkt) {
+	void *p;
+	p = pkt->dataItems;
+	memset(pkt, 0, sizeof(struct xbee_pkt));
+	pkt->dataItems = p;
+}
+
 /* add a key, or get hold of a key if it exists already */
 int xbee_pktAddKey(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, struct pkt_infoKey **retKey, void (*freeCallback)(void*)) {
 	struct pkt_infoKey *p;

pkt.h
@@ -30,6 +30,7 @@ struct pkt_infoKey {
 };
 
 struct xbee_pkt *xbee_pktAlloc(void);
+void xbee_pktClean(struct xbee_pkt *pkt);
 
 int xbee_pktAddKey(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, struct pkt_infoKey **retKey, void (*freeCallback)(void*));
 int xbee_pktAddInfo(struct xbee *xbee, struct xbee_pkt *pkt, char *key, int id, void *data, void (*freeCallback)(void*));

rx.c
@@ -210,7 +210,7 @@ int _xbee_rxHandlerThread(struct xbee_pktHandler *pktHandler) {
 		xbee_log(2,"Processing packet @ %p", buf);
 		
 		/* clear out the connection and packet structs - the handler should fill them in */
-		memset( pkt, 0, sizeof(struct xbee_pkt));
+		xbee_pktClean(pkt);
 		memset(&con, 0, sizeof(struct xbee_con));
 		/* call the handler, it should fill in con and pkt */
 		if ((ret = pktHandler->handler(data->xbee, pktHandler, 1, &buf, &con, &pkt)) != 0) {